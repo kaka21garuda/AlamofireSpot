bbfcb604359e9820c0a4cf717f71c8d4dc654786
Buka Cakrawala <kaka10garuda@gmail.com>
2016-10-10 11:48:11 -0700

    JSON is readable

--- Spot/ViewController.swift
+++ Spot/ViewController.swift
@@ -7,19 +7,56 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var searchURL = "https://api.spotify.com/v1/search?q=Justin+Bieber&type=track&offset=20"
+    typealias standardJSON = [String: AnyObject]
+    //
+    var namesArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        alamofire(url: searchURL)
+        
     }
-
+    
+    func alamofire(url: String) {
+        Alamofire.request(url).responseJSON(completionHandler: {
+            response in
+            self.parseData(JSONData: response.data!)
+            
+        })
+    }
+    
+    func parseData(JSONData: Data) {
+        do {
+            var readableJSON = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as! standardJSON
+            if let tracks = readableJSON["tracks"] as? standardJSON {
+                if let items = tracks["items"] as? standardJSON {
+                    for i in 0..<items.count {
+                        let item = items[i]
+                        let name = item["name"]
+                        namesArray.append(name)
+                        
+                    }
+                }
+            }
+        } catch {
+            print(error)
+        }
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
-
+    
+  
 }
 
